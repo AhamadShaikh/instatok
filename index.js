19fd99d0e3ecb539e06f5ee6d7f40b22151fc58b
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-07-13 17:00:42 +0530

    done

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 const userRouter = require("./routes/userRoutes")
 const postRouter = require("./routes/postRoute")
+require("dotenv").config()
+
+app.use(express.json());
+app.use(express.text());
+app.use(cors());
 
 const connection = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://Ahamad786:Ahamad4820@cluster0.p86c9yw.mongodb.net/Marvel?retryWrites=true&w=majority`)
+        await mongoose.connect(process.env.MONGO_URL)
     } catch (error) {
         console.log(error)
     }
 }
 
-app.use(express.json());
+
 
 app.use("/user", userRouter)
 app.use("/post", postRouter)
